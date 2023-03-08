702612f2444524d8d537a359c929a3fcabf564ae
moradi2558 <moradi2558@gmail.com>
2023-03-08 10:43:36 +0330

    slug and not foundc404

--- first_web/home/models.py
+++ first_web/home/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from accounts.models import User
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length = 200)
     create = models.DateTimeField(auto_now_add = True)
     update = models.DateTimeField(auto_now = True)
     image = models.ImageField(upload_to = 'Category')
+    slug = models.SlugField(allow_unicode=True,unique = True,null = True,blank = True)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home:category',args=[self.slug,self.id])
 
 
 class Product(models.Model):
@@ -23,11 +28,14 @@ class Product(models.Model):
     update = models.DateTimeField(auto_now = True)
     image = models.ImageField(upload_to = 'product')
     available = models.BooleanField(default = True)
+    slug = models.SlugField(allow_unicode=True,unique = True,null = True,blank = True)
     
     def __str__(self):
         return self.name
 
-    
+    def get_absolute_url(self):
+        return reverse('home:detail',args=[self.slug,self.id])
+
     
     
     @property
