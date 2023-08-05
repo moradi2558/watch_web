3b36805ae342732f56993e0ae4b600112525779b
hesam <hesam82rhm@gmail.com>
2023-08-05 10:20:22 +0330

    bg color changed ; change, fav, orders page styled

---
+++ first_web/accounts/static/change.js
@@ -0,0 +1,27 @@
+let allLabel = document.querySelectorAll("label")
+allLabel.forEach(function(lable){
+lable.innerHTML = lable.innerHTML.slice(0,lable.innerHTML.length-1)
+})
+
+let allp = document.querySelectorAll("p");
+allp.forEach(function(p){
+p.insertAdjacentHTML("beforeend",`<i class="fas fa-eye"></i>`)
+})
+
+let allIcon = document.querySelectorAll(".fa-eye")
+let flagIcon = false
+allIcon.forEach(function(icon){
+icon.addEventListener("click",function(e){
+    if (flagIcon) {
+        e.target.classList.add("fa-eye")
+        e.target.classList.remove("fa-eye-slash")
+        e.target.previousElementSibling.setAttribute("type","password")
+        flagIcon = false
+    } else {
+        e.target.classList.add("fa-eye-slash")
+        e.target.classList.remove("fa-eye")
+        e.target.previousElementSibling.setAttribute("type","text")
+        flagIcon = true
+    }
+})
+})
