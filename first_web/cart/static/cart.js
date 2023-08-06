480753fc5798e055a8fceaac21b12baaec5b0ba7
hesam <hesam82rhm@gmail.com>
2023-08-06 14:20:55 +0330

    cart page styled

---
+++ first_web/cart/static/cart.js
@@ -0,0 +1,49 @@
+let minusIcons = document.querySelectorAll(".fa-minus")
+
+minusIcons.forEach(function (minus) {
+    let Qty = minus.parentElement.nextElementSibling.firstElementChild.innerHTML;
+    if(Qty == 1){
+        minus.classList.remove("fa-minus")
+        minus.classList.add("fa-trash")
+        minus.classList.add("text-danger")
+        minus.style.fontSize = "medium"
+    }
+})
+
+
+let $ = document
+
+const continu = $.querySelector('.continue')
+const modalParent = $.querySelector('.modal-parent')
+const x = $.querySelector('.X')
+const sectionElem = $.querySelector('.section')
+
+console.log(window.innerWidth)
+
+function showModal() {
+    modalParent.style.display = 'block'
+    sectionElem.style.filter = 'blur(10px)'
+    window.addEventListener("resize",function(){
+        if (window.innerWidth > 766) {
+            sectionElem.style.filter = 'blur(0px)'
+        }else{
+            sectionElem.style.filter = 'blur(10px)'
+        }
+    })
+}
+
+function hideModalWithX() {
+    modalParent.style.display = 'none'
+    sectionElem.style.filter = 'blur(0px)'
+}
+
+function hideModalWithEsc(event) {
+    if (event.keyCode === 27) {
+        modalParent.style.display = 'none'
+        sectionElem.style.filter = 'blur(0px)'
+    }
+}
+
+continu.addEventListener('click', showModal)
+x.addEventListener('click', hideModalWithX)
+document.body.addEventListener('keyup', hideModalWithEsc)
